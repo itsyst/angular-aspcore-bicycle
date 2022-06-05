682d8f335280fff6ab286e9efdedad87287c8556
Khaled El Hamzi <contact@elhamzi.me>
2022-06-05 11:07:10 +0200

    Apply constraints

--- Models/Make.cs
+++ Models/Make.cs
@@ -1,10 +1,13 @@
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace angular_aspcore_bicycle.Models;
-
 public class Make
 {
+    [Key]
     public int Id { get; set; }
+    [Required]
+    [StringLength(255)]
     public string Name { get; set; } = "";
     public ICollection<Model> Models { get; set; } = new Collection<Model>();
 }
--- Models/Model.cs
+++ Models/Model.cs
@@ -1,9 +1,13 @@
 
-namespace angular_aspcore_bicycle.Models;
+using System.ComponentModel.DataAnnotations;
 
+namespace angular_aspcore_bicycle.Models;
 public class Model
 {
+    [Key]
     public int Id { get; set; }
+    [Required]
+    [StringLength(255)]
     public string Name { get; set; } = "";
     public Make Make { get; set; } = new();
 
