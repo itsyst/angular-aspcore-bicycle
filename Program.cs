bb369663adf12fd8d7eaa4929f66e058235c5930
Khaled El Hamzi <contact@elhamzi.me>
2022-06-05 11:08:37 +0200

    Add context service to the container.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using angular_aspcore_bicycle.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<BicycleDbContext>(options => options
+.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+b => b.MigrationsAssembly(typeof(BicycleDbContext).Assembly.FullName)
+).EnableSensitiveDataLogging());
 
 var app = builder.Build();
 
