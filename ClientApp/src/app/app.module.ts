7f1d1ebcdac2b29486b792602966ec6e2bbfd3ef
Khaled El Hamzi <contact@elhamzi.me>
2022-06-06 16:24:39 +0200

    Create bicycle route.

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -26,6 +26,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'bicycles/new', component: BicycleFormComponent },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
     ])
